a7dfca73a13f1c67668cd8c8298e48b4ef4abe8b
ElianBorda <eliancamiloalejandro@gmail.com>
2023-12-22 22:27:49 -0300

    cambios

--- backend/ialquila-backend/src/main/kotlin/ar/com/ialquila/controller/AlquilerControllerREST.kt
+++ backend/ialquila-backend/src/main/kotlin/ar/com/ialquila/controller/AlquilerControllerREST.kt
@@ -6,6 +6,7 @@ import ar.com.ialquila.model.Alquiler
 import ar.com.ialquila.service.AlquilerService
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -17,8 +18,16 @@ import org.springframework.web.bind.annotation.RestController
 class AlquilerControllerREST(private val alquilerService: AlquilerService) {
 
     @GetMapping("/alquileres")
-    fun todosLosAlquileres() = alquilerService.getAll().map { alquiler -> AlquilerDTO.desdeModeloAJson(alquiler)  }
+    fun todosLosAlquileres() = alquilerService.getAll().map { alquiler -> AlquilerDTO.desdeModeloAJson(alquiler) }
 
-    @PostMapping("/guardar-alquiler")
+    @PostMapping("/guardar/alquiler")
     fun guardarAlquiler(@RequestBody alquiler: AlquilerDTO) = alquilerService.save(alquiler.desdeJsonAModelo())
+
+    @PostMapping("/guardar/alquileres")
+    fun guardarAlquileres(@RequestBody alquileres: List<AlquilerDTO>) =
+        alquilerService.saveAll(alquileres.map { a -> a.desdeJsonAModelo() })
+
+    @GetMapping("/alquiler/{id}")
+    fun obtenerAlquiler(@PathVariable id: String) = AlquilerDTO.desdeModeloAJson(alquilerService.getById(id))
+
 }
