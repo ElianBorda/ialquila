bfd1a74bb114f2d011b80cac2721f46c3e591ae1
ElianBorda <eliancamiloalejandro@gmail.com>
2024-02-04 00:48:57 -0300

    se mejoro el scraping 3

--- backend/scrips/propratiscrapingdatabuilder.py
+++ backend/scrips/propratiscrapingdatabuilder.py
@@ -1,6 +1,7 @@
 from scrapingdatabuilder import *
 from pager import *
 from soupcreator import *
+from websitedata import * 
 
 
 class ProperatiScrapingDataBuilder(ScrapingDataBuilder):
@@ -15,11 +16,35 @@ class ProperatiScrapingDataBuilder(ScrapingDataBuilder):
         except Exception as e:
             print("No hay una siguiente pagina")
             return None 
-        
-        
     
-    def getdatacards(html):
-        return super().getdatacards()
+    def getdatacards(self, soup):
+        datacards = []
+        for card in self._getcards(soup):
+            datacards.append(self.getdatacard(card))
+        return datacards
     
-    def getdatacard(html):
-        return super().getdatacard()
+    def getdatacard(self, soup):
+        return WebsiteData(
+            self._getdatatitle(soup),
+            self._getdatadesc(soup),
+            
+        )
+    
+    def _getcards(self, soup):
+        return soup.find_all('div', class_='listing-card')
+    
+    def _getdatatitle(self, soup):
+        return soup.find('div', class_='listing-card__title').text
+    
+    def _getdatadesc(self, soup):
+        return ""
+
+    def _getdatatitle(self, soup):
+        return soup.find('div', class_='listing-card__title').text
+    
+    def _getdataprice(self, soup):
+        info = (soup.find('div', class_='listing-card__information')).find('div', class_='listing-card__information-main')
+    
+        return (info.find('div', class_='listing-card__price-wrapper')).find('div', class_='price').text
+    
+        
